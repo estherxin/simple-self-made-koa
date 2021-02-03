ad1e5ac2be5b19619e116e2b0894533f5dd19677
esther <zhangxinfang@wondersgroup.com>
2021-02-03 17:32:06 +0800

    json

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const koa = require("./koa/application")
 // const router = require('koa-router')();
 const app = new koa()
 app.use(async (ctx,next)=>{
-    ctx.body = ctx.body +"1a"
+    ctx.body = {a:1}
+    
     // console.time("timer");
     await next()
     ctx.body = ctx.body +"3c"
--- koa/response.js
+++ koa/response.js
@@ -4,6 +4,13 @@ module.exports = {
         return this._body
     },
     set body(newVal){
+        if(typeof newVal !== 'string'){
+            if(Object.prototype.toString.call(newVal) === '[object Object]') {
+                 newVal = JSON.stringify(newVal)
+                 return this._body = newVal
+            }
+            newVal = newVal.toString()
+        }
         this._body = newVal
     }    
 }
